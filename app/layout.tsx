f568bc14277da013bb0844e8afdef986533c285d
laxxkafley <116989353+laxxkafley@users.noreply.github.com>
2024-10-18 02:57:04 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,16 +4,6 @@ import "./globals.css";
 import Head from "../component/head"
 import Foot from "../component/foot";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
